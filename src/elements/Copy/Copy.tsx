0f1c084a1902c45c088ff4b58ea4f850a1158540
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-05-14 18:26:06 -0400

    styel copy section

--- src/elements/Copy/Copy.tsx
+++ src/elements/Copy/Copy.tsx
@@ -3,13 +3,13 @@ import "./Copy.css";
 export default function Copy() {
   return (
     <div className="copy-wrapper">
-      <h2 className="copy-product-category">Perfume</h2>
+      <h2 className="copy-product-category">PERFUME</h2>
       <h1 className="copy-product-name">Gabrielle Essence Eau de Parfum</h1>
       <p className="copy-product-description">
         A floral, solar, and voluptuous interpretation by Olivier Polge,
         Perfumer-Creator for the House of CHANEL.
       </p>
-      <p>
+      <p className="copy-price-wrapper">
         <span className="copy-discounted-price">$149.99</span>
         <span className="copy-base-price">$169.99</span>
       </p>
